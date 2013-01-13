acc796446e558d25b94a31fc9b7c2867cdc11c4d
SAITO Jun <jsaito@xopowo.info>
2013-01-13 20:09:48 +0900

    rename arguments

--- lib/citlunch/client.rb
+++ lib/citlunch/client.rb
@@ -8,12 +8,12 @@ module CITLunch
 
     API_ENDPOINT = "https://udon.upsilo.net/citlunch/api"
 
-    def get_today(name)
-      get(name, Date.today)
+    def get_today(place)
+      get(place, Date.today)
     end
 
     def get(name, date)
-      url = "#{API_ENDPOINT}/#{name}/#{date.strftime("%Y-%m-%d")}/menu.json"
+      url = "#{API_ENDPOINT}/#{place}/#{date.strftime("%Y-%m-%d")}/menu.json"
       begin
         open(url) {|f| JSON.parse(f.read).map {|r| Menu.new(r)}}
       rescue
