250a2ef0a44438b34bc469d2fbcb975083cf0409
SAITO Jun <jsaito@xopowo.info>
2013-02-06 19:36:59 +0900

    raise ArgumentError if illegal place specified

--- lib/pebbles/citlunch/client.rb
+++ lib/pebbles/citlunch/client.rb
@@ -7,12 +7,14 @@ module CITLunch
   class Client
 
     API_ENDPOINT = "https://udon.upsilo.net/citlunch/api"
+    PLACES = [:tsuda_dining, :shiba_dining, :shiba_kissa]
 
     def get_today(place)
       get(place, Date.today)
     end
 
     def get(place, date)
+      raise ArgumentError.new ("unknown place: #{place}") unless PLACES.include? place
       url = "#{API_ENDPOINT}/#{place}/#{date.strftime("%Y-%m-%d")}/menu.json"
       begin
         open(url) {|f| JSON.parse(f.read).map {|r| Menu.new(r)}}
